23d1f4e1a3cc8f07b00a96f57ff8ab3b3c36e853
Tetiana Hrytsaienko <kortatiana89@gmail.com>
2017-10-03 18:25:44 +0300

    Animation finished

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -2,25 +2,56 @@ import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
-    constructor(elements, offset) {
-        this.itemsToReveal = elements;
-        this.offsetPercentage = offset;
+    constructor() {
+        this.itemsPhoneNumbers = $('.header__phone');
+        this.itemsAdvantages = $('.advantages__description');
+        this.itemsModels = $('.ref-models__list');
+        this.itemsProblems = $('.problems__description');
         this.hideInitially();
         this.createWaypoints();
     }
     hideInitially() {
-        this.itemsToReveal.addClass('reveal-item');
+        this.itemsModels.addClass('reveal-item');
     }
     createWaypoints() {
         var correctThis = this;
-        this.itemsToReveal.each(function () {
+        this.itemsPhoneNumbers.each(function () {
+            var currentItem = this;
+            new Waypoint({
+                element: currentItem,
+                handler: function () {
+                    $(currentItem).addClass('animated bounceInDown');
+                }
+            });
+        });
+        this.itemsAdvantages.each(function () {
+            var currentItem = this;
+            new Waypoint({
+                element: currentItem,
+                handler: function () {
+                    $(currentItem).addClass('animated fadeInRight');
+                },
+                offset: '85%'
+            });
+        });
+        this.itemsModels.each(function () {
             var currentItem = this;
             new Waypoint({
                 element: currentItem,
                 handler: function () {
                     $(currentItem).addClass('reveal-item--is-visible');
                 },
-                offset: correctThis.offsetPercentage
+                offset: '85%'
+            });
+        });
+        this.itemsProblems.each(function () {
+            var currentItem = this;
+            new Waypoint({
+                element: currentItem,
+                handler: function () {
+                    $(currentItem).addClass('animated bounceInLeft');
+                },
+                offset: '50%'
             });
         });
     }
